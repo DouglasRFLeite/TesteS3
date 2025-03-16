0e9c5d67d14d2f44f2286be1aa02f1f8e22759db
Douglas Leite <douglas.rochedo@gmail.com>
2025-03-16 15:33:58 -0300

    Spring Initialize

---
+++ testes3/src/test/java/com/douglas/testes3/Testes3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.testes3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testes3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
